c93557b4601296f4b43f212814d80021f0762d98
Halverson <haljas@dunwoody.edu>
2019-02-04 11:58:26 -0600

    created Person class

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace project1
 {
@@ -6,7 +8,24 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("Welcome to the 2019 NFL Draft.");
+            Console.WriteLine("This simple program will help you to make your selections within the confines of your salary cap.");
+            Console.WriteLine("Please press any key to begin");
+            Console.ReadLine();
+            */
         }
     }
+    class Player
+    {
+        public string Position { get; set; }
+        public string Rank { get; set; }
+        public string Name { get; set; }
+        public string College { get; set; }
+        public double requestedSalary { get; set; }
+        public bool playerSelected { get; set; }
+        
+    }
 }
+
+
